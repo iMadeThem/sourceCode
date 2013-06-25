717dea4b3273fdf8e7653d6d4904843d93ebcf86
imadeit <yong.he@icloud.com>
2013-06-26 00:56:00 +0900

    Update gitPreCommitHook.sh

--- SH/gitPreCommitHook.sh
+++ SH/gitPreCommitHook.sh
@@ -3,7 +3,8 @@ set -e
 set -u
 set -o pipefail
 
-REPO_ROOT="$(git rev-parse --show-toplevel)"
+THIS_DIR="$( dirname "${BASH_SOURCE[0]}" )"
+REPO_ROOT=$( cd "$THIS_DIR/../../" && pwd )
 echo $REPO_ROOT
 
 for cmd in "${REPO_ROOT}"/tools/git-hooks/pre-commit.d/*

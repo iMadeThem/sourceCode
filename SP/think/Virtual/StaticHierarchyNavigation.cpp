66e207fc3a90fd0b310d9d1ff61bb31ad7f8a274
Leon He <yong.he1982@gmail.com>
2013-09-06 08:27:45 +0900

    [SP] Static hierarchy cast.

---
+++ SP/think/Virtual/StaticHierarchyNavigation.cpp
@@ -0,0 +1,54 @@
+/**
+ * StaticHierarchyNavigation.cpp -- Navigating class hierarchies with static_cast.
+ *
+ * Copyright (c) Leon (yong.he1982(at)gmail.com)
+ * Date : 2013-09-06 [Friday, 249]
+ *
+ * Build:
+ *   g++ StaticHierarchyNavigation.cpp -o StaticHierarchyNavigation
+ */
+
+#include <iostream>
+#include <typeinfo>
+
+using namespace std;
+
+class Shape {
+    public:
+        virtual ~Shape() {};
+};
+
+class Circle : public Shape {};
+
+class Square : public Shape {};
+
+class Other {};
+
+int main() {
+    Circle c;
+    Shape* s = &c; // Upcast: normal and OK
+    // More explicit but unnecessary:
+    s = static_cast<Shape*>(&c);
+    // (Since upcasting is such a safe and common
+    // operation, the cast becomes cluttering)
+    Circle* cp = 0;
+    Square* sp = 0;
+    // Static Navigation of class hierarchies
+    // requires extra type information:
+    if(typeid(s) == typeid(cp)) // C++ RTTI
+        cp = static_cast<Circle*>(s);
+    if(typeid(s) == typeid(sp))
+        sp = static_cast<Square*>(s);
+    if(cp != 0)
+        cout << "It's a circle!" << endl;
+    if(sp != 0)
+        cout << "It's a square!" << endl;
+    // Static navigation is ONLY an efficiency hack;
+    // dynamic_cast is always safer. However:
+    // Other* op = static_cast<Other*>(s);
+    // Conveniently gives an error message, while
+    Other* op2 = (Other *) s;
+    // does not
+
+    return 0;
+} ///:~

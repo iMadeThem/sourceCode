b93009face61810ae9111e1e5bcdd430a3024a02
Leon He <leon.he@gree.net>
2013-09-05 10:03:20 +0900

    [SP] Single rooted hierarchy.

---
+++ SP/think/Virtual/OStack.h
@@ -0,0 +1,53 @@
+/**
+ * OStack.h -- Using a singly-rooted hierarchy.
+ *
+ * Copyright (c) Leon (yong.he1982(at)gmail.com)
+ * Date : 2013-09-05 [Thursday, 248]
+ */
+
+#ifndef _HEADER_OSTACK_H_
+#define _HEADER_OSTACK_H_
+
+class Object {
+    public:
+        virtual ~Object() = 0;
+};
+
+// Required definition:
+
+inline Object::~Object() {}
+
+class Stack {
+    struct Link {
+        Object* data;
+        Link* next;
+        Link(Object * dat, Link * nxt) : data(dat), next(nxt) {}
+    }* head;
+
+    public:
+        Stack() : head(0) {}
+
+        ~Stack() {
+            while(head)
+                delete pop();
+        }
+
+        void push(Object* dat) {
+            head = new Link(dat, head);
+        }
+
+        Object* peek() const {
+            return head ? head->data : 0;
+        }
+
+        Object* pop() {
+            if(head == 0) return 0;
+            Object* result = head->data;
+            Link* oldHead = head;
+            head = head->next;
+            delete oldHead;
+            return result;
+        }
+};
+
+#endif /* ~_HEADER_OSTACK_H_ */
